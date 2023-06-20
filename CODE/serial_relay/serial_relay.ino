966c558d06c189a75455e0e0f577639e849e648f
reedb <reedb@reedbement.com>
2023-06-20 08:53:59 -0700

    zero buffer to aid debugging

--- CODE/serial_relay/serial_relay.ino
+++ CODE/serial_relay/serial_relay.ino
@@ -4,127 +4,69 @@
 
 // The following provide a mapping from Arduino digital pin to relay
 // board control pins INx. The pins must be in sequential order.
-// Input control pins on relay board are active low. 
 //
-#define RELAY_BOARD_IN1_PIN     2	// Arduino Digital Pins D2 - D9
-#define NUM_RELAY_BOARD_IN_PINS 8	// Number of input pins on realy board
+#define RELAY_BOARD_IN1_PIN     2
+#define NUM_RELAY_BOARD_IN_PINS 8
 
-// Serial command protocol at 9600 baud:
-// SOT, Eight characters of zero or one, EOT
-// Start of text (SOT): 'b',  98, 0x62
-// End   of text (EOT): '\n', 10, 0x0A
-//
-#define SOT 'b'
-#define EOT '\n'
-#define NUM_COMMAND_BYTES (1 + 8 + 1)
-
-// States for serial command RX state machine (bRxState).
-//
-#define IDLE    0
-#define RX_BITS 1
-#define RX_EOT  3
-#define DONE    4
-
-
-char g_szDbg[80];
-
-SoftwareSerial mySerial(10, 11); // RX - D10 not used, TX - D11 sends debug log at 9600 baud
+SoftwareSerial mySerial(10, 11); // RX, TX
 
 void flush_serial_rc(void)
 {
     while (Serial.available() > 0) Serial.read();
 }
 
-byte receive_command_byte(byte bRxState, char *pszRxCmd)
-{ 
-	digitalWrite(LED_BUILTIN, HIGH);
-	static word wIndex;
-	byte b = Serial.read();
-	if (isPrintable(b)) mySerial.write(b);
-	else {
-		sprintf(g_szDbg, "<0x%02X>", b);
-		mySerial.print(g_szDbg);
-	}
-	
-	switch (bRxState) {
-        case IDLE:				// Waiting for SOT
-			if (b == SOT) {
-				pszRxCmd[0] = b;
-				wIndex = 0;
- 		        mySerial.println("\r\nRX_BITS");
-				return RX_BITS;
-			}
-            break;
-        case RX_BITS:			// Receive binary bits
-			if ((b == '0') || (b == '1')) {
-				pszRxCmd[++wIndex] = b;
-				if (wIndex >= 8) {
- 				    mySerial.println("\r\nRX_EOT");
-					return RX_EOT;
-				}
-				return RX_BITS;
-			}
-            break;
-        case RX_EOT:			// Receive EOT
-			if (b == EOT) {
-				pszRxCmd[9] = b;
-				mySerial.println("\r\nDONE");
-				return DONE;
-			}
-            break;
-        default:
-			mySerial.println("\r\nUnknown state!");
-            assert(0);
-    }
-    mySerial.println("\r\nProtocol Error. Back to IDLE");
-    memset(pszRxCmd, 0, sizeof(pszRxCmd));
-	flush_serial_rc();
-	return IDLE;	
-}
-
 void setup() {
-	digitalWrite(LED_BUILTIN, LOW);
-	pinMode(LED_BUILTIN, OUTPUT);
+  // Open serial communications and wait for port to open:
+  Serial.begin(9600);
+  while (!Serial);
 
-	// Open serial communications and wait for port to open:
-	Serial.begin(9600);
-	while (!Serial);
-	
-	// Define pins as output in inactive state.
-	for (int i = 0;  i < NUM_RELAY_BOARD_IN_PINS; i++) {
-	  digitalWrite(i + RELAY_BOARD_IN1_PIN, HIGH);
-	  pinMode(i + RELAY_BOARD_IN1_PIN, OUTPUT);
-	}
-	
-	// Set the data rate for the SoftwareSerial port (debug log).
-	mySerial.begin(9600);
-	mySerial.println("Relay control firmware, version 1.2");
+  // Define pins as output in inactive state.
+  for (int i = 0;  i < NUM_RELAY_BOARD_IN_PINS; i++) {
+      digitalWrite(i + RELAY_BOARD_IN1_PIN, HIGH);
+      pinMode(i + RELAY_BOARD_IN1_PIN, OUTPUT);
+  }
+
+  // set the data rate for the SoftwareSerial port
+  mySerial.begin(9600);
+  mySerial.println("Relay control firmware, version 1.0");
 }
 
 void loop()
 {
-    static char szRxCmd[NUM_COMMAND_BYTES + 1]; // Command + NULL 
-    static byte bRxState = IDLE;
-    
-    if (Serial.available()) bRxState = receive_command_byte(bRxState, szRxCmd);
-    if (bRxState == DONE) {    
-        errno = 0;
-        byte b = (byte) (strtol(&szRxCmd[1], NULL, 2) & 0xFF);
-        if (errno == 0 ) {
-	        // Binary format conversion and termination checks passed, set relays.
-	        byte bMask = 0b00000001;
-	        for (int i = 0;  i < NUM_RELAY_BOARD_IN_PINS; i++) {
-	            digitalWrite(i + RELAY_BOARD_IN1_PIN, (b & bMask) ? LOW:HIGH);
-	            bMask <<= 1; 
-	        }
-	        mySerial.println("Relays set");
-	        Serial.write(szRxCmd);
+    static char szRxCommand[8 + 2 + 1]; // Eight characters zero|one + CR + LF + NULL 
+
+    if (Serial.available() >= (8 + 2)) {
+        memset(szRxCommand, 0, sizeof(szRxCommand));
+        mySerial.write("Received: ");
+        for (int i = 0;  i < 8 + 2; i++) {
+            szRxCommand[i] = Serial.read();
+            mySerial.write(szRxCommand[i]);
         }
-		else {
-			sprintf(g_szDbg, "Binary format error, strtol: %d", errno);
-			mySerial.println(g_szDbg); Serial.println(g_szDbg);
-		}
-		bRxState = IDLE;
+        assert(szRxCommand[8 + 2] == '\0');
+        // Check for terminators
+        if ((szRxCommand[8] != '\r') or (szRxCommand[9] != '\n')) {
+            mySerial.println("Invalid terminator");
+            Serial.write("Error terminator\r\n");
+            flush_serial_rc();
+            return;
+        }
+        errno = 0;
+        unsigned char b = (unsigned char) (strtol(szRxCommand, NULL, 2) & 0xFF);
+        if (errno != 0 ) {
+            mySerial.println("Binary format error");
+            Serial.write("Error format\r\n");
+            flush_serial_rc();
+            return;
+        }
+
+        // Binary format conversion and termination checks passed, set relays.
+        unsigned char bMask = 0b00000001;
+        for (int i = 0;  i < NUM_RELAY_BOARD_IN_PINS; i++) {
+            digitalWrite(i + RELAY_BOARD_IN1_PIN, (b & bMask) ? LOW:HIGH);
+            bMask <<= 1; 
+        }
+        
+        Serial.write(szRxCommand);
     }
-    digitalWrite(LED_BUILTIN, LOW);
 }
+
